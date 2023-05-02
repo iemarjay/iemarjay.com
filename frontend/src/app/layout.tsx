6c1e527b90019aad70faa4f17cbc2682a46ddfb5
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2023-05-02 13:17:26 +0400

    add open graph

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import React from "react";
+import Head from "next/head";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -20,6 +21,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="Joseph Emmanuel (iemarjay)" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Full-stack software engineer, blockchain engineer and serial builder\nI have a wide range of technical experience, including building multiple products, winning numerous hackathons, and creating software solutions for various industries"
+        />
+        <meta property="og:url" content="https://iemarjay.com" />
+        <meta
+          property="og:image"
+          content="/media/hire-emmanuel-joseph-iemarjay.png"
+        />
+      </Head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
