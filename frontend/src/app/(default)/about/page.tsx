75d2f86f4d23bf0e0a01d7c9f2578e007d9e840b
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2023-05-06 09:30:00 +0400

    Create page.tsx

---
+++ frontend/src/app/(default)/about/page.tsx
@@ -0,0 +1,55 @@
+export default function About() {
+  return (
+    <div className="mt-8 max-w-4xl">
+      <p className="mb-6">
+        I&#39;m a Fullstack Software/Blockchain Engineer with a passion for
+        developing innovative solutions that solve real-world problems. With
+        over 5 years of experience in software development, I&#39;ve worked on a
+        range of projects spanning Fintech, blockchain, e-commerce, and
+        enterprise software.
+      </p>
+      <p className="mb-6">
+        My expertise lies in ReactJS, NextJS, JavaScript, ES6+, NodeJS,
+        Typescript, Solidity, EthersJS, Hardhat, Go lang, Echo, Fiber, Java,
+        Maven, Elasticsearch, Tailwind CSS, Bootstrap, Webpack, Babel, VueJS,
+        NuxtJS, Docker, Jenkins, PHP, Laravel, GraphQL, HTML5, CSS3, and SASS.
+      </p>
+      <p className="mb-6">
+        I&#39;m proud to have led the development of PiggyFi, a groundbreaking
+        platform on the Celo blockchain that won the Binance Africa Hackathon
+        (Defi Category) and Celo Hackathon. PiggyFi enables users to send money
+        across Africa within an hour using cUSD or local bank transfer. I also
+        wrote the Smart contract and contributed to the development of OpenLand,
+        a fractionalized NFT platform for buying, selling, and earning returns
+        on land assets on the blockchain, deployed live on Moonriver.
+      </p>
+      <p className="mb-6">
+        In my previous role as a Software Engineer at Wikimedia Foundation, I
+        contributed to an Elasticsearch-based search engine that powers
+        Wikipedia and its sister projects, which indexes ~10 billion documents
+        across ~1000 wikis in ~300 different languages. I upgraded Elasticsearch
+        plugins, including 7 internally developed plugins and contributions to 3
+        open source plugins, making them compatible with Elasticsearch 7 and
+        published on Maven Central.
+      </p>
+      <p className="mb-6">
+        As the Tech lead at Decasoft Technologies, I led a team of 4 in building
+        Decafinance, a Fintech loan and savings PAAS that currently serves over
+        2,000 ride-hailing drivers with over ₦18,421,200 (~ $40,000) in
+        transactions. I integrated several banking APIs to automate loan
+        disbursements, withdrawals, and savings collection on behalf of
+        customers.
+      </p>
+      <p className="mb-6">
+        I hold a BSc in Mathematics / Computer Science from Olabisi Onabanjo
+        University and continue to pursue new technologies and methodologies to
+        stay at the forefront of software development.
+      </p>
+      <p className="mb-6">
+        If you&#39;re looking for a talented Fullstack Software Engineer who can
+        deliver innovative solutions that meet your business needs, feel free to
+        contact me. Thank you for visiting my portfolio website!
+      </p>
+    </div>
+  );
+}
