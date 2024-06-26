8da4f9003ccb66e4179f7c251c9f3bfd740fdb30
joseph emmanuel kayode (iemarjay) <emmanueljoseph@fitted.ng>
2024-06-26 14:31:20 +0100

    Update actions.ts

--- frontend/src/app/actions.ts
+++ frontend/src/app/actions.ts
@@ -8,7 +8,6 @@ export async function getBlogPostsForHome(): Promise<Post[]> {
 
 export async function getPaginatedBlogPosts(page = 1, take = 20): Promise<Post[]> {
   const start = ((page || 1) - 1) * take;
-  console.log('start', start, 'take', take, 'page', page)
   const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') + `/blog-posts?_start=${start}&_limit=${take}&_sort=published_at:DESC`);
   return await response.json();
 }
