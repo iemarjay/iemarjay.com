5c85b7c94cd98666d1f1d1e52c12c6e54a1b2d1e
joseph emmanuel kayode (iemarjay) <emmanueljoseph@fitted.ng>
2024-07-10 21:57:42 +0100

    debug

--- frontend/src/app/actions.ts
+++ frontend/src/app/actions.ts
@@ -1,12 +1,15 @@
 'use server';
+console.log(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, ''), 'first' )
 
 
 export async function getBlogPostsForHome(): Promise<Post[]> {
+  console.log(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') )
   const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') + '/blog-posts?_limit=10&_sort=published_at:DESC');
   return await response.json();
 }
 
 export async function getPaginatedBlogPosts(page = 1, take = 20): Promise<Post[]> {
+  console.log(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') )
   const start = ((page || 1) - 1) * take;
 
   const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') + `/blog-posts?_start=${start}&_limit=${take}&_sort=published_at:DESC`);
@@ -14,6 +17,7 @@ export async function getPaginatedBlogPosts(page = 1, take = 20): Promise<Post[]
 }
 
 export async function getBlogPostBySlug(slug: string): Promise<Post> {
+  console.log(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') )
   const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_BASE_URL?.replace(/\/+$/, '') + `/blog-posts?slug=${slug}`);
   return (await response.json())?.[0];
 }
