a8b28b4e978840566cf19d97593d302e9ff87701
joseph emmanuel kayode (iemarjay) <emmanueljoseph@fitted.ng>
2024-09-28 12:45:23 +0100

    Update sitemap.ts

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -4,7 +4,7 @@ import {getAllBlogPostForSitemap} from "@/app/actions";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const BASE_URL = 'https://iemarjay.com';
   const posts = (await getAllBlogPostForSitemap() ?? []).map((post) => ({
-    url: `${BASE_URL}/blog/${post.slug}`,
+    url: `${BASE_URL}/post/${post.slug}`,
     priority: 0.8,
     changeFrequency: "weekly",
     lastModified: post.updated_at,
@@ -13,7 +13,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {url: `${BASE_URL}/`, lastModified: new Date()},
     {url: `${BASE_URL}/about`},
-    {url: `${BASE_URL}/blog`, lastModified: new Date()},
+    {url: `${BASE_URL}/post`, lastModified: new Date()},
     ...posts
   ];
 }
